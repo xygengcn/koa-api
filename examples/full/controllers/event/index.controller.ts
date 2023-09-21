89d8320b2a5cce0f4ec51ef37ad2d5a150c0cb32
xygengcn <xygengcn@qq.com>
2023-09-22 00:11:03 +0800

    chore: version 1.0.9

--- examples/full/controllers/event/index.controller.ts
+++ examples/full/controllers/event/index.controller.ts
@@ -1,4 +1,4 @@
-import { EventStream, Get } from '@/decorators';
+import { EventStream, Get, Post } from '@/decorators';
 import { PassThrough } from 'stream';
 import { Controller, Param } from '../../../../src';
 
@@ -13,13 +13,47 @@ export default class EventStreamController {
      */
     @EventStream()
     @Get()
-    public test(@Param.Stream() stream: PassThrough) {
+    public get(@Param.Stream() stream: PassThrough, @Param.Query('userId') userId: string) {
         const time = setInterval(() => {
-            stream.write(`data: ${new Date()}\n\n`);
-            console.log('1111');
+            stream.write(`data: ${userId}\n\n`);
+            console.log('[event-get]');
         }, 1000);
         stream.once('close', () => {
+            console.log('[event-get] close');
             clearInterval(time);
         });
+    }
+
+    /**
+     * 支持text/event-stream数据流
+     *
+     * 类似chatGPT返回
+     *
+     * @param stream
+     */
+    @EventStream()
+    @Post()
+    public post(@Param.Stream() stream: PassThrough, @Param.Body('userId') userId: string) {
+        const time = setInterval(() => {
+            stream.write(`data: ${userId}\n\n`);
+            console.log('[event-post]');
+        }, 1000);
+        stream.once('close', () => {
+            console.log('[event-post] close');
+            clearInterval(time);
+        });
+    }
+
+    /**
+     * 支持text/event-stream数据流
+     *
+     * 类似chatGPT返回
+     *
+     * @param stream
+     */
+    @EventStream()
+    @Post()
+    public error(@Param.Stream() stream: PassThrough, @Param.Body('userId') userId: string) {
+        throw Error('测试错误');
     }
 }
